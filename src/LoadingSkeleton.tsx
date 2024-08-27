7f21af53fca17f6d48551c93c0d827ee68ea46b1
miapiar <meri.lacouturiere@gmail.com>
2024-08-27 23:36:18 +0100

    added badge button card and skeleton compos

---
+++ src/LoadingSkeleton.tsx
@@ -0,0 +1,35 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardFooter,
+} from './components/ui/card';
+
+import { Skeleton } from './components/ui/skeleton';
+
+const LoadingSkeleton = () => {
+  return (
+    <main className=" w-full">
+      <div className="grid grid-cols-3 gap-8">
+        {'abcedrfhi'.split('').map(i => (
+          <Card key={i} className="flex flex-col justify-between">
+            <CardHeader className="flex-row gap-4 items-center">
+              <Skeleton className="w-12 h-12 rounded-full" />
+              <Skeleton className="h-6 flex-grow" />
+            </CardHeader>
+            <CardContent>
+              <Skeleton className="h-4 flex-grow mt-4" />
+              <Skeleton className="h-4 flex-grow mt-4" />
+              <Skeleton className="h-4 w-1/2 mt-4" />
+            </CardContent>
+            <CardFooter>
+              <Skeleton className="w-28 h-12 " />
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default LoadingSkeleton;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,15 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Badge } from '@/components/ui/badge';
+
+import LoadingSkeleton from '@/LoadingSkeleton';
 
 interface Recipes {
   id: string;
@@ -19,6 +25,7 @@ interface Recipes {
 
 function Home() {
   const [recipes, setRecipes] = useState<Recipes[]>([]);
+  const [isloading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const getRecipes = async () => {
@@ -28,7 +35,8 @@ function Home() {
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
-
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        setIsLoading(true);
         const data: Recipes[] = await response.json();
         setRecipes(data);
       } catch (error) {
@@ -45,19 +53,38 @@ function Home() {
       </nav>
 
       <main>
-        <div className="grid grid-cols-3 gap-8">
-          {recipes.map(recipe => (
-            <Card>
-              <CardHeader>
-                <CardTitle>{recipe.title}</CardTitle>
-                <CardDescription>{recipe.description}</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <img src={`../../public/img/${recipe.image}`} />
-              </CardContent>
-            </Card>
-          ))}
-        </div>
+        {isloading ? (
+          <div className="grid grid-cols-3 gap-8">
+            {recipes.map(recipe => (
+              <Card key={recipe.id} className="flex flex-col justify-between">
+                <CardHeader className="flex-row gap-4 items-center">
+                  <Avatar>
+                    <AvatarImage
+                      src={`img/${recipe.image}`}
+                      alt="recipe image"
+                    />
+                    <AvatarFallback>{recipe.title.slice(0, 2)}</AvatarFallback>
+                  </Avatar>
+                  <div>
+                    <CardTitle>{recipe.title}</CardTitle>
+                    <CardDescription>
+                      {recipe.time} mins to cook
+                    </CardDescription>
+                  </div>
+                </CardHeader>
+                <CardContent>
+                  <p>{recipe.description}</p>
+                </CardContent>
+                <CardFooter className="flex justify-between">
+                  <Button>View recipe</Button>
+                  {recipe.vegan && <Badge variant="secondary">Vegan!</Badge>}
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
+        ) : (
+          <LoadingSkeleton />
+        )}
       </main>
     </>
   );
